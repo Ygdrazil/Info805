7945a0429059e5461a06125575a73931ae87f216
Ygdrazil <vfz1204@gmail.com>
2023-03-30 15:45:42 +0200

    [🛠] WIP

--- I805_TP2_Lambada/src/main/java/fr/usmb/m1isc/compilation/tp/BinaryNode.java
+++ I805_TP2_Lambada/src/main/java/fr/usmb/m1isc/compilation/tp/BinaryNode.java
@@ -1,63 +1,114 @@
 package fr.usmb.m1isc.compilation.tp;
 
-public class BinaryNode<T> {
-    private T nodeValue;
+public class BinaryNode {
+    private String nodeValue;
 
     private BinaryNode leftChild;
     private BinaryNode rightChild;
 
-    public BinaryNode(T nodeValue) {
+    private TerminalType terminalType;
+
+    public BinaryNode(String nodeValue, TerminalType terminalType) {
         this.nodeValue = nodeValue;
         this.leftChild = null;
         this.rightChild = null;
+        this.terminalType = terminalType;
     }
 
-    public BinaryNode(T nodeValue, BinaryNode leftChild, BinaryNode rightChild) {
+    public BinaryNode(String nodeValue, BinaryNode leftChild, BinaryNode rightChild) {
         this.nodeValue = nodeValue;
         this.leftChild = leftChild;
         this.rightChild = rightChild;
+        terminalType = TerminalType.NONE;
     }
 
-    public BinaryNode(T nodeValue, BinaryNode leftChild) {
+    public BinaryNode(String nodeValue, BinaryNode leftChild) {
         this.nodeValue = nodeValue;
         this.leftChild = leftChild;
         this.rightChild = null;
-    }
-
-
-
-    public T getNodeValue() {
-        return nodeValue;
-    }
-
-    public void setNodeValue(T nodeValue) {
-        this.nodeValue = nodeValue;
-    }
-
-    public BinaryNode getLeftChild() {
-        return leftChild;
-    }
-
-    public void setLeftChild(BinaryNode leftChild) {
-        this.leftChild = leftChild;
-    }
-
-    public BinaryNode getRightChild() {
-        return rightChild;
-    }
-
-    public void setRightChild(BinaryNode rightChild) {
-        this.rightChild = rightChild;
+        terminalType = TerminalType.NONE;
     }
 
     @Override
     public String toString() {
         if(leftChild == null)
-            return nodeValue.toString();
+            return nodeValue;
 
         if(rightChild == null)
             return "(" + nodeValue + " " + leftChild + ")";
 
         return "(" + nodeValue + " " + leftChild + " " + rightChild + ")";
+    }
+
+    public void toAsm() {
+
+        // Noeud sans fils
+        if(this.terminalType != TerminalType.NONE ) {
+            switch (this.terminalType) {
+                case ENTIER -> System.out.println("mov eax, " + nodeValue);
+                case VARIABLE -> System.out.println("mov "+nodeValue+", eax");
+                case NIL -> System.err.println("nil");
+                case INPUT -> System.err.println("input");
+            }
+        }
+
+        if(this.leftChild != null) {
+            // Noeud avec un seul fils
+            if(this.rightChild == null) {
+                switch (nodeValue) {
+                    case "-" -> System.out.println("-"+this.leftChild);
+                    case "OUTPUT" -> System.err.println("OUTPUT");
+                    case "()" -> this.leftChild.toAsm();
+                }
+            }
+            // Noeud avec deux fils
+            else {
+                switch (nodeValue) {
+                    case "+"-> {
+                        this.leftChild.toAsm();
+                        System.out.println("push eax");
+                        this.rightChild.toAsm();
+                        System.out.println("pop ebx");
+
+                        System.out.println("add eax, ebx");
+                    }
+                    case "-" -> {
+                        this.leftChild.toAsm();
+                        System.out.println("push eax");
+                        this.rightChild.toAsm();
+                        System.out.println("pop ebx");
+
+                        System.out.println("sub eax, ebx");
+                    }
+                    case "*" -> {
+                        this.leftChild.toAsm();
+                        System.out.println("push eax");
+                        this.rightChild.toAsm();
+                        System.out.println("pop ebx");
+
+                        System.out.println("mul eax, ebx");
+                    }
+                    case "/" -> {
+                        this.leftChild.toAsm();
+                        System.out.println("push eax");
+                        this.rightChild.toAsm();
+                        System.out.println("pop ebx");
+
+                        System.out.println("div ebx, eax");
+                        System.out.println("mov eax, ebx");
+                    }
+                    case "LET" -> {
+                        this.rightChild.toAsm();
+                        this.leftChild.toAsm();
+                        System.out.println("mov eax, "+ this.leftChild.nodeValue);
+                        System.out.println("push eax");
+                    }
+                }
+            }
+        }
+    }
+
+    enum TerminalType {
+        NONE, ENTIER, VARIABLE, NIL, INPUT
     }
 }
--- I805_TP2_Lambada/src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
+++ I805_TP2_Lambada/src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
@@ -15,9 +15,30 @@ public class Main {
 	    yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
 	@SuppressWarnings("deprecation")
 	parser p = new parser (yy);
-	Symbol s = p.parse( );
+	Symbol s = p.parse();
 	BinaryNode arbre = (BinaryNode) s.value;
 	System.out.println(arbre.toString());
-    }
+	/*	DATA SEGMENT
+			prixHt DD
+			prixTtc DD
+		DATA ENDS
+		CODE SEGMENT
+			mov eax, 200
+			mov prixHt, eax
+			mov eax, prixHt
+			push eax
+			mov eax, 119
+			pop ebx
+			mul eax, ebx
+			push eax
+			mov eax, 100
+			pop ebx
+			div ebx, eax
+			mov eax, ebx
+			mov prixTtc, eax
+	    CODE ENDS*/
+
+	arbre.toAsm();
+	}
 
 }
